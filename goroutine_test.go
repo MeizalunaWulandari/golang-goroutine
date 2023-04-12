8360bbde612171a03ccd49d362c75cbff0843db7
Meizaluna Wulandari <axelmaxi2810@gmail.com>
2023-04-12 10:43:18 +0800

    Membuat 100K goroutine

--- goroutine_test.go
+++ goroutine_test.go
@@ -15,9 +15,22 @@ func TestCreateGoRoutine(t *testing.T){
 	fmt.Println("Ups")
 	time.Sleep(1 * time.Second)
 }
+
+func DisplayNumber(number int){
+	fmt.Println("Display ke", number)
+}
+
+func TestManyGoroutine(t *testing.T){
+	for i := 0; i < 100000; i++{
+		go DisplayNumber(i)
+	}
+	time.Sleep(5 * time.Second)
+}
+
 /**
  * Untuk membuat go routine cukup dengan menambahkan keyword go sebelum menjalankan function
  * Namun jika menggunakan go routine jika function tersebut belum selesai dan program telah selesai
  * maka function tersebut akan dimatikan secara otamatis, maka dari itu pastikan untuk membuat jeda 
  * agar function tersebut dapat diselasaikan sebelum program berhenti
+ * Goroutine tidak adakan berguna pada function yang mengembalikan return value
  */
