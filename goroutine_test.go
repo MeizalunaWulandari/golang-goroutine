5c2f56e046a91f37d795e5ee459a6d31d2b49bcd
Meizaluna Wulandari <axelmaxi2810@gmail.com>
2023-04-12 10:58:11 +0800

    Channel

--- goroutine_test.go
+++ goroutine_test.go
@@ -33,4 +33,14 @@ func TestManyGoroutine(t *testing.T){
  * maka function tersebut akan dimatikan secara otamatis, maka dari itu pastikan untuk membuat jeda 
  * agar function tersebut dapat diselasaikan sebelum program berhenti
  * Goroutine tidak adakan berguna pada function yang mengembalikan return value
+ * 
+ * PENGENALAN CHANNEL
+ * Channel adalah tempat komunikasi secara synchronous yang bisa dilakukan oleh goroutine
+ * Di channel terdapat pengirim dan penerima, pengirim dan penerima biasanya adalah goroutine yang berbeda
+ * Saat melakukan pengiriman data ke channel, goroutine akan ter-block sampai ada goroutine yang menerima data tersebut
+ * Simplenya mirip async await pada JS
+ * Channel hanya bisa menampung satu data, jika ingin menambahkan data lagi, harus menunggu data yang ada di channel diambil 
+ * Channel hanya bisa menerima satu jenis data 
+ * Channel bisa diambil lebih dari satu goroutine
+ * Channel harus di close jika tidak digunakan atau bisa menyebabkan memory leak
  */
