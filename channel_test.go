d714d31ab51e9528726f4667c1c5ad1b25f21072
Meizaluna Wulandari <axelmaxi2810@gmail.com>
2023-04-14 12:10:36 +0800

    Select Channel

--- channel_test.go
+++ channel_test.go
@@ -91,6 +91,33 @@ func TestRangeChannel(t *testing.T){
     fmt.Println("Selesai")
 }
 
+func TestSelectChannel(t *testing.T){
+    channel1 := make(chan string)
+    channel2 := make(chan string)
+
+    defer close(channel1)
+    defer close(channel2)
+
+    go GiveMeResponse(channel1)
+    go GiveMeResponse(channel2)
+
+    counter := 0
+
+    for {
+        select {
+        case data := <-channel1:
+            fmt.Println("Data dari channel 1 ", data)
+            counter++
+        case data := <-channel2:
+            fmt.Println("Data dari channel 2 ", data)
+            counter++
+        }
+        if counter == 2 {
+            break
+        }
+    }
+}
+
 
 
 /** MEMBUAT CHANNEL
@@ -133,4 +160,11 @@ func TestRangeChannel(t *testing.T){
  * dari channel
  * Ketika sebuah channel di close(), maka perulangan tersebut akan berhenti 
  * ini lebih sederhana dari pada kita melakukan pengecekan secara manual
+ * 
+ * SELECT CHANNEL
+ * Kadang ada kasus dimana kita membuat beberapa channel dan menjalankan beberapa goroutine 
+ * lalu kita ingin mendapatkan data dari semua channel tersebut
+ * Untuk melakukan ini kita bisa menggunakan select 
+ * dengan select channel kita bisa memilih channel tercepat dari beberapa channel, jika data
+ * datang secara bersamaan dibeberapa channel, maka akan terpilih secara random
  */
