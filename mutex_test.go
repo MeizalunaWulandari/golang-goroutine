db0f5234c50de3cdb2b1b6d1def2021d2841cbed
Meizaluna Wulandari <axelmaxi2810@gmail.com>
2023-04-15 10:14:34 +0800

    Deadlock

--- mutex_test.go
+++ mutex_test.go
@@ -60,6 +60,63 @@ func TestRWMutex(t *testing.T){
 
 }
 
+type UserBalance struct{
+    sync.Mutex
+    Name string
+    Balance int
+}
+
+func (user *UserBalance) Lock(){
+    user.Mutex.Lock()
+}
+
+func (user *UserBalance) Unlock(){
+    user.Mutex.Unlock()
+}
+
+func (user *UserBalance) Change(amount int){
+    user.Balance = user.Balance + amount
+}
+
+
+func Transfer(user1 *UserBalance, user2 *UserBalance, amount int){
+    user1.Lock()
+    fmt.Println("Lock user1", user1.Name)
+    user1.Change(-amount)
+
+    time.Sleep(1 * time.Second)
+
+    user2.Lock()
+    fmt.Println("Lock user2", user2.Name)
+    user2.Change(amount)
+
+    time.Sleep(1 * time.Second)
+
+    user1.Unlock()
+    user2.Unlock()
+}
+
+func TestDeadlock(t *testing.T){
+    user1 := UserBalance{
+        Name : "Andini",
+        Balance : 1000000,
+    }
+    user2 := UserBalance{
+        Name : "Luna",
+        Balance : 1000000,
+    }
+
+    go Transfer(&user1, &user2, 100000)
+    go Transfer(&user2, &user1, 200000)
+    
+    time.Sleep(1 * time.Second)
+
+    fmt.Println("User ", user1.Name, ", Balance", user1.Balance)
+    fmt.Println("User ", user2.Name, ", Balance", user2.Balance)
+
+}
+
+
 /** SYNC.MUTEX
  * Mutex(Mutual Exclusion) 
  * Untuk mengatasi masalah race condition, di golang terdapat sebuah struct bernama sync.Mutex
@@ -79,4 +136,9 @@ func TestRWMutex(t *testing.T){
  * Di golang telah disediakan struct RWMutex untuk menangani hal ini, dimana mutex jenis ini memiliki dua 
  * dua lock, lock untuk Read dan Lock untuk Write
  * RWMutex hanya diperlukan ketika membuat data yang akan diakses oleh beberapa goroutine
+ * 
+ * DEADLOCK
+ * Deadlock adalah masalah yang biasa terjadi ketika membuat aplikasi yang concurrent dan paraller
+ * Deadlock sendiri terjadi karena sebuah goroutine saling menunggu sehingga tidak ada satupu 
+ * goroutine yang berjalan, hal ini biasanya disebabkan oleh locking
  */
